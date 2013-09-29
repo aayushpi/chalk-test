7f76a30af23158432ba2118b767ff271f49630d6
Aayush Iyer <aayushiyer@Aayushs-MacBook-Pro.local>
2013-09-29 16:24:54 -0700

    updated formula

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ $('.calc').click(function(){
     var inc, kgs, result;
     inc = (parseInt($('#ft').val(), 10) * 12) + parseInt($('#inc').val(),10);
     kgs = parseInt($('#kg').val(), 10);
-    var male = 50 + 2.3*(inc - 60);
-    var female = 45.5 + 2.3*(inc - 60);
+    var male = 52 + 1.9*(inc - 60);
+    var female = 49 + 1.7*(inc - 60);
     var mdiff = kgs - male;
     var fdiff = kgs - female;
     if(Math.abs(mdiff) < Math.abs(fdiff)){
@@ -17,6 +17,8 @@ $('.calc').click(function(){
     $('.results span').html(result);
     $('.results').fadeIn(200);
 });
+
+
 
 
 
